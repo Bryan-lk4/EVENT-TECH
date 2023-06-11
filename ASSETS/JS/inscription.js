d5f75031a1741709dfae89f1eec28fb0140f1c68
Bryan-lk4 <bryanleke04@gmail>
2023-06-11 14:19:58 +0100

    Les index de bases et les images de bases

---
+++ ASSETS/JS/inscription.js
@@ -0,0 +1,9 @@
+
+const logo=document.querySelector(".logo");
+
+
+logo.addEventListener("click", () => {
+    const back=document.querySelector(".formulaire");
+    back.style.backgoundColor("#948d8ded");
+
+})
