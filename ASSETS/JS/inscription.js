3a0ec8ac33bff482dc79a373c6b7a7389dbc85f1
Bryan lk <bryanleke04@gmail.com>
2023-06-24 19:01:39 +0100

    login modif

--- ASSETS/JS/inscription.js
+++
@@ -1,9 +0,0 @@
-
-const logo=document.querySelector(".logo");
-
-
-logo.addEventListener("click", () => {
-    const back=document.querySelector(".formulaire");
-    back.style.backgoundColor("#948d8ded");
-
-})
